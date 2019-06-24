f262b5487f32d84f6fc2cbdae74a42feb110d971
Erik the Viking <erik@hhd.com.au>
2019-06-24 20:40:20 +1000

    add constructor for match

--- models/match.ts
+++ models/match.ts
@@ -9,8 +9,17 @@ export type CurrentMatch = TennisSet[]
 export class Match {
   private currentMatch: CurrentMatch
   private players: [Player, Player]
+  private scoreReporter: ScoreReporter = new ScoreReporter()
+  private scoreKeeper: ScoreKeeper = new ScoreKeeper()
+  private umpire: Umpire = new Umpire()
+  private requiredSets: number
+  private wonBy: number | null = null
 
-  constructor(player1: string, player2: string) {
+  constructor(
+    player1: string,
+    player2: string,
+    { requiredSets }: { requiredSets: number } = { requiredSets: 1 }
+  ) {
     // validate inputs
     // the order which the players were passed in matters because (as per the spec of the test) the
     // way the scores are reported make no sense unless you know the input order
@@ -19,6 +28,7 @@ export class Match {
       new Player({ name: player1, index: 0 }),
       new Player({ name: player2, index: 1 }),
     ]
+    this.requiredSets = requiredSets
     this.currentMatch = [new TennisSet()]
   }
 }
