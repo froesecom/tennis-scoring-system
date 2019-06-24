737c0f2aa7f2b28ecc5d1bde04de17f04a61ab69
Erik the Viking <erik@hhd.com.au>
2019-06-24 19:40:54 +1000

    init a match before each test

--- __tests__/models/match.test.ts
+++ __tests__/models/match.test.ts
@@ -1,3 +1,11 @@
+import { Match } from '../../models/match'
+
+let match: Match = new Match('player1', 'player2')
+
+beforeEach(() => {
+  match = new Match('player1', 'player2')
+})
+
 test('the truth', () => {
   expect(true).toBeTruthy()
 })
